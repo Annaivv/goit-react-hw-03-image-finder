f60d8ff9ec18127e20417355f250ee06c0b09a73
Annaivv <annashalya@gmail.com>
2023-02-12 01:26:06 +0200

    basic structure from HW added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import { GlobalStyle } from './GlobalStyle';
+import ImageGallery from './ImageGallery/ImageGallery';
 import { Layout } from './Layout';
+import LoadMoreBtn from './LoadMoreButton/Button';
 import Searchbar from './Searchbar/Searchbar';
 
 export const App = () => {
   return (
     <Layout>
       <Searchbar />
+      <ImageGallery />
+      <LoadMoreBtn />
       <GlobalStyle />
     </Layout>
   );
