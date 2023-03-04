829829702d8da07e8661eda35543d0d781095084
Annaivv <annashalya@gmail.com>
2023-03-04 21:04:35 +0200

    working version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,8 @@ import { GlobalStyle } from './GlobalStyle';
 import ImageGallery from './ImageGallery/ImageGallery';
 import { Layout } from './Layout';
 import Searchbar from './Searchbar/Searchbar';
-// import { addImage } from 'services/api';
+import { addImage } from 'services/api';
 import Loader from './Loader/Loader';
-import { addImageFirstPage, addImageNextPages } from 'services/api';
 
 export default class App extends Component {
   state = {
@@ -20,25 +19,11 @@ export default class App extends Component {
 
   async componentDidUpdate(_, prevState) {
     try {
-      if (prevState.query !== this.state.query) {
-        this.setState({ isLoading: true });
-        const data = await addImageFirstPage(this.state.query);
-        if (data.hits.length === 0) {
-          toast.error('No results for your search');
-          this.setState({ isLoading: false });
-          return;
-        }
-        this.setState({
-          results: data.hits,
-          isLoading: false,
-        });
-      }
-
       if (
-        prevState.page !== this.state.page &&
-        prevState.query === this.state.query
+        prevState.page !== this.state.page ||
+        prevState.query !== this.state.query
       ) {
-        const data = await addImageNextPages(this.state.query, this.state.page);
+        const data = await addImage(this.state.query, this.state.page);
 
         this.setState(prevState => ({
           results: [...prevState.results, ...data.hits],
@@ -51,13 +36,17 @@ export default class App extends Component {
   }
 
   handleFormSubmit = query => {
-    this.setState({ query });
+    this.setState({
+      query,
+      isLoading: false,
+      page: 1,
+      results: [],
+      error: null,
+    });
   };
 
   loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   render() {
--- src/services/api.js
+++ src/services/api.js
@@ -3,26 +3,7 @@ import axios from 'axios';
 const BASIC_API = 'https://pixabay.com/api/';
 const API_KEY = '30900325-2c40b95e1611f9496716f72a9';
 
-export const addImageFirstPage = async value => {
-  try {
-    const response = await axios.get(`${BASIC_API}`, {
-      params: {
-        q: value,
-        page: 1,
-        key: API_KEY,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        per_page: 12,
-      },
-    });
-
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const addImageNextPages = async (value, page) => {
+export const addImage = async (value, page) => {
   try {
     const response = await axios.get(`${BASIC_API}`, {
       params: {
